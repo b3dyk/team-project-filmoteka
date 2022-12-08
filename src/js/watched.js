900a3c7e1e6670f756a73dc621ead40bd764e2ee
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2022-12-08 03:42:04 +0200

    discard changes in watched.js

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,8 +1,6 @@
 import { Movies } from './fetch';
-import { APIKey } from './apikey';
-import { markupMyLibrary } from './markup';
 
-// const APIKey = 'e0e51fe83e5367383559a53110fae0e8';
+const APIKey = 'e0e51fe83e5367383559a53110fae0e8';
 
 const refs = {
   libraryList: document.querySelector('#library-list'),
@@ -15,7 +13,7 @@ const refs = {
   modalContent: document.querySelector('.modal__content'),
 };
 
-// let GENRES = [0];
+let GENRES = [0];
 
 // Слухачі подій
 
@@ -28,15 +26,11 @@ try {
 } catch (error) {}
 try {
   refs.queueBtn.addEventListener('click', removeLibraryListWatched);
-} catch (error) {
-  console.log('error ', error);
-}
+} catch (error) {}
 
 Start();
 async function Start() {
-  let watchedFilm = [];
-  let watchedFilmId = [];
-  // await getGenres();
+  await getGenres();
 
   await checkWatched();
 
@@ -44,15 +38,15 @@ async function Start() {
 }
 
 // Формування переліку жанрів
-// async function getGenres() {
-//   const movies = new Movies(APIKey);
-//   try {
-//     GENRES = await movies.getGenres();
-//     console.log('GENRES ', GENRES);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
+async function getGenres() {
+  const movies = new Movies(APIKey);
+  try {
+    GENRES = await movies.getGenres();
+    console.log('GENRES ', GENRES);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 //  Витяг з LocalStorage
 let watchedFilm = [];
@@ -136,54 +130,53 @@ async function addWatched(event) {
 }
 
 // Створення контенту My library watched
-function addLibraryListWatched() {
-  refs.libraryList.innerHTML = '';
-  refs.watchedBtn.classList.add('button--active');
-  refs.queueBtn.classList.remove('button--active');
+async function addLibraryListWatched() {
+  try {
+    refs.libraryList.innerHTML = '';
+    refs.watchedBtn.classList.add('button--active');
+    refs.queueBtn.classList.remove('button--active');
+  } catch (error) {}
 
   if (localStorage.getItem('watched')) {
-    markupMyLibrary(watchedFilm);
-    // for (const film of watchedFilm) {
-    //   try {
-    //     refs.libraryList.insertAdjacentHTML('beforeend', markupCard(film));
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // }
+    for (const film of watchedFilm) {
+      try {
+        refs.libraryList.insertAdjacentHTML('beforeend', markupCard(film));
+      } catch (error) {}
+    }
   }
 }
 
 // Створення однієї картки
-// function markupCard(imgObj) {
-//   const URI = `https://image.tmdb.org/t/p/w500${imgObj.poster_path}`;
-//   const date = new Date(imgObj.release_date);
-//   const genres = markupGenres(imgObj.genres);
+function markupCard(imgObj) {
+  const URI = `https://image.tmdb.org/t/p/w500${imgObj.poster_path}`;
+  const date = new Date(imgObj.release_date);
+  const genres = markupGenres(imgObj.genres);
 
-//   return `<li class="grid__item filmoteka__item">
-// 			<div class="card" data-id="${imgObj.id}">
-//                 <div class="card__img">
-// 					<img src="${URI}" alt="${imgObj.title}">
-// 				</div>
-//                     <div class="card__wrapper">
-//                         <h2 class="card__title title">${imgObj.title}</h2>
-//                         <p class="card__desc">${genres} | ${date.getFullYear()}
-//                         <span class="card__vote">
-//                             ${imgObj.vote_average.toFixed(1)}
-//                         </span>
-//                         </p>
-//                     </div>
-//                 </div>
-// 			</li>`;
-// }
+  return `<li class="grid__item filmoteka__item" data-id="${imgObj.id}">
+			<div class="card" data-id="${imgObj.id}">
+                <div class="card__img">
+					<img src="${URI}" alt="${imgObj.title}">
+				</div>
+                    <div class="card__wrapper">
+                        <h2 class="card__title title">${imgObj.title}</h2>
+                        <p class="card__desc">${genres} | ${date.getFullYear()}
+                        <span class="card__vote">
+                            ${imgObj.vote_average.toFixed(1)}
+                        </span>
+                        </p>
+                    </div>
+                </div>
+			</li>`;
+}
 
 // Створення жанрів в одну картку
-// function markupGenres(genre_ids) {
-//   let genres = [];
-//   for (const genre of genre_ids) {
-//     genres.push(genre.name);
-//   }
-//   return genres.join(', ');
-// }
+function markupGenres(genre_ids) {
+  let genres = [];
+  for (const genre of genre_ids) {
+    genres.push(genre.name);
+  }
+  return genres.join(', ');
+}
 
 // Видалення переглянутого контенту
 function removeLibraryListWatched() {
